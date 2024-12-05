56c5e4b9a8dbfa2dff3e5df1fae3d6bde4b17092
Pelayogm <pelagrmenendez12@gmail.com>
2024-12-05 20:51:34 +0100

    v1.8.6 Actualizados los objetivos y quedan los cambios apuntados

--- Tarea_2_ADT_Pelayogm/src/main/java/proyectoPokemonADT/Main.java
+++ Tarea_2_ADT_Pelayogm/src/main/java/proyectoPokemonADT/Main.java
@@ -25,23 +25,28 @@ public class Main {
 
 //ERRORES / REVISAR
 //_____________________________________________
-//DESLIAR EL CODIGO / Comentar el codigo
+//DESLIAR EL CÓDIGO / Comentar el código
 //ESTRUCTURAR EN PAQUETES (SEMI-COMPLETADO)
 //Corregir script de la BD
 
 //TAREA 2
 //TorneoDTO lleva una ListaDeCombates que se tiene que hacer en torneoServices (COMPLETADA)
 //ESTRUCTURA DAO (COMPLETADA)
-//Coger datos del torneo en la base de datos (Apuntarse a un torneo)
+//Coger datos del torneo en la base de datos (Apuntarse a un torneo) (COMPLETADO)
 
 //DUDAS
 //_____________________________________________________________________________________
-//El entrenador lleva una lista de torneos. (Hacer Tabla TorneoEntrenador), para el torneo inicial.
-//Hacer que se reflejen los torneos en la bd y exportar carnet
+//El entrenador lleva una lista de torneos. (Hacer Tabla TorneoEntrenador), para el torneo inicial. (COMPLETADO)
+//Hacer que se reflejen los torneos en la bd y exportar carnet (COMPLETADO)
 
-//Al entrenador se le enseñan los torneos disponibles y escoge el que quiere
-//Y el administrador de torneos al hacer login se le enseñan todos los torneos que administra y exporta el que quiera.
+//Al entrenador se le enseñan los torneos disponibles y escoge el que quiere (COMPLETADO)
+//Y el administrador de torneos al hacer login se le enseñan todos los torneos que administra y exporta el que quiera. (COMPLETADO)
+//El torneo se exporta en .txt y se sale por pantalla y se guarda el id del torneo Creado con el adminDeTorneos. (COMPLETADO)
+
+//En un torneo un entrenador solo puede combatir 2 veces y en un torneo hay 3 combates.
+//Hacer flush en el exportar
+//Añadir los combates al XML
 
 //TorneoDTO tiene listaDeParticipantes
-//El torneo se exporta en .txt y se sale por pantalla y se guarda el id del torneo Creado con el adminDeTorneos.
-//En un torneo un entrenador solo puede combatir 2 veces y en un torneo hay 3 combates.
+
+
